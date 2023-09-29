87b453fb2cf542e3d3ba59facf520bfa1a9e2db1
tslavchevsqa <temelko.slavchev@scalefocus.com>
2023-09-29 14:19:29 +0300

    project structure

---
+++ WeddingsAutomation/Utilities/WebDriverManager.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeddingsAutomation.Utilities
+{
+    public static class WebDriverManager
+    {
+        private static IWebDriver _driver;
+
+        public static IWebDriver Driver
+        {
+            get
+            {
+                if (_driver == null)
+                    _driver = new ChromeDriver();
+                
+                return _driver;
+            }
+        }
+
+        public static void QuitDriver()
+        {
+            _driver.Quit();
+        }
+    }
+}
