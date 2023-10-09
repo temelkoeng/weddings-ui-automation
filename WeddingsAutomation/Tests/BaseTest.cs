6417453fcc9ce9a2ceee8241fd39036bb626834a
Stoycho Enchev (EXT-Nokia) <stoycho.enchev.ext@nokia.com>
2023-10-09 16:18:44 +0300

    Fixed the webdrivermanager

--- WeddingsAutomation/Tests/BaseTest.cs
+++ WeddingsAutomation/Tests/BaseTest.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,7 +17,7 @@ namespace WeddingsAutomation.Tests
         [OneTimeSetUp]
         public void Setup()
         {
-            driver = new WebDriverController();    //PLS UPDATE?!??!?!?
+            driver = new WebDriverController(new ChromeDriver());
         }
 
         [OneTimeTearDown]
